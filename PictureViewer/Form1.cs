777b2cb82264ad5286baf3b97f76c1ff637f89a4
asterism1030 <rlaguswl1030@gmail.com>
2023-10-25 17:32:38 +0900

    버튼 클릭 이벤트 기능 구현

--- PictureViewer/Form1.cs
+++ PictureViewer/Form1.cs
@@ -19,7 +19,15 @@ namespace PictureViewer
 
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
         {
-
+            // 사진을 늘이기/일반 모드로 변경함
+            if(checkBox1.Checked)
+            {
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+            else
+            {
+                pictureBox1.SizeMode = PictureBoxSizeMode.Normal;
+            }
         }
 
         private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
@@ -29,22 +37,31 @@ namespace PictureViewer
 
         private void showBtn_Click(object sender, EventArgs e)
         {
-
+            // 파일 오픈창을 열고 사용자가 선택한 이미지를 불러옴
+            if(openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Load(openFileDialog1.FileName);
+            }
         }
 
         private void clearBtn_Click(object sender, EventArgs e)
         {
-
+            // 이미지 비우기
+            pictureBox1.Image = null;
         }
 
         private void bgBtn_Click(object sender, EventArgs e)
         {
-
+            // 색상표 불러오기
+            if(colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.BackColor = colorDialog1.Color;
+            }
         }
 
         private void closeBtn_Click(object sender, EventArgs e)
         {
-
+            this.Close();
         }
     }
 }
